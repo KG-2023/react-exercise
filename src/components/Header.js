eb4a601503236c167449bdfe9cff34a97807e298
Rodrigo Lopez <scdtp2019@gmail.com>
2023-09-02 15:31:37 +0800

    add class based example

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 // import React from 'react'
-
+/* Arrow function syntax
 const Header = () => {
   return (
     <div>
@@ -9,5 +9,38 @@ const Header = () => {
     </div>
   )
 }
+*/
 
-export default Header
+/* Function declaration syntax*/
+/*
+function Header() {
+  return (
+    <div>
+      <header>
+      <h1 style={{ textAlign: 'center' }}>RBSL To-Do-App</h1>
+    </header></div>
+  )
+}
+
+*/
+
+
+/* Class Based Component Syntax*/
+
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1 style={{ textAlign: 'center' }}>RBSL To-Do-App</h1>
+        </header>
+      </div>
+    )
+  }
+}
+
+
+
+// export default Header
