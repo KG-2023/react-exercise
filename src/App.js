e8c5ac4a00238eb8de5b5fb40e0ba35c477e2628
Edgar Pateño <edgarpateno80@gmail.com>
2023-09-02 16:18:55 +0800

    create title props

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
    
     <div className="container">
-      <Header title="Hello Melvin!" />
+      <Header />      
     </div>
 
    /*
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 
 //Arrow function syntax
-const Header = (props) => {
+const Header = ({ title}) => {
   return (
     <div>
         <header>
             <h1 style={{textAlign:'center'}}>To-Do-App</h1>
-            <h1 style={{textAlign:'center'}}>{props.title}</h1>
+            <h2 style={{textAlign:'center'}}>{title}</h2>
         </header>
     </div>
   )
 }
 
+Header.defaultProps = {
+  title: 'Task Tracker'
+}
 
 export default Header
+
