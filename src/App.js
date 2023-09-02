6fb42bf3e5276316738a13fa9064e22555fed0bd
Edgar Pateño <edgarpateno80@gmail.com>
2023-09-02 15:59:48 +0800

    create Props

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
    
     <div className="container">
-      <Header />
+      <Header title="Hello Melvin!" />
     </div>
 
    /*
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,15 @@
-// import React from 'react'
-/* Arrow function syntax
-const Header = () => {
+
+//Arrow function syntax
+const Header = (props) => {
   return (
     <div>
         <header>
             <h1 style={{textAlign:'center'}}>To-Do-App</h1>
+            <h1 style={{textAlign:'center'}}>{props.title}</h1>
         </header>
     </div>
   )
 }
-*/
-
-/* Function declaration syntax*/
-/*
-function Header() {
-  return (
-    <div>
-      <header>
-      <h1 style={{ textAlign: 'center' }}>RBSL To-Do-App</h1>
-    </header></div>
-  )
-}
-
-*/
 
 
-/* Class Based Component Syntax*/
-
-import React, { Component } from 'react'
-
-export default class Header extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-          <h1 style={{ textAlign: 'center' }}>RBSL To-Do-App</h1>
-        </header>
-      </div>
-    )
-  }
-}
-
-
-
-// export default Header
+export default Header
