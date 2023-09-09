8e68e40e836fb2da664fa5bc23025a7e0e3ea088
Rodrigo Lopez <scdtp2019@gmail.com>
2023-09-09 11:15:21 +0800

    Setup add task Post request

--- src/App.js
+++ src/App.js
@@ -30,13 +30,17 @@ function App() {
 }
  
  // ADD TASK 
-  const addTask = (task) =>{
-    // console.log(task);
-    const id = Math.floor(Math.random() * 10000 + 1);
-    // console.log(id);
+  const addTask = async (task) =>{
+    const res = await fetch(`http://localhost:5000/tasks`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
 
-    const newTask = {id, ...task}
-    setTasks([...tasks, newTask]);
+    const data = await res.json();
+    setTasks([...tasks, data]);
   }
 
   // TOGGLE THE TASK
